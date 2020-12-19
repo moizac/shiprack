e8640d755fe8226b893fc0feb133c1a24a7a8e8a
Moizac <izfa87aremania@gmail.com>
2020-12-19 21:43:26 +0700

    update commit

--- resources/views/lacak.blade.php
+++ resources/views/lacak.blade.php
@@ -3,6 +3,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Ship Tracker</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-icon.png">
@@ -29,7 +30,7 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href=""><img class="user-avatar rounded-circle" src="{{ asset('style/images/ship.png') }}"></a>
+                <a class="navbar-brand" href=""><img class="user-avatar rounded-circle" src="{{ asset('style/images/shiprack2.png') }}"></a>
                 <a class="navbar-brand hidden" href="">SR</a>
             </div>
 
